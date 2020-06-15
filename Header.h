b7a2a4e7564a8868ba02a4cb2852ea2644c3566a
stein4444 <eysmont.vitya@gmail.com>
2020-06-15 22:16:45 +0300

    first commit

---
+++ Header.h
@@ -0,0 +1,31 @@
+#pragma once
+#include<iostream>
+#include<string>
+using namespace std;
+
+class Book
+{
+private:
+	string bookName;
+	string author;
+	string publisher;
+	int year;
+	float rating;
+public:
+	
+
+private:
+
+};
+
+class Library
+{
+private:
+	Book* books;
+public:
+	
+
+private:
+
+};
+
